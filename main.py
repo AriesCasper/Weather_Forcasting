6bc37f1e4597fa2f79ed434eab15f13b2587dfc3
AriesCasper <63998386+AriesCasper@users.noreply.github.com>
2023-05-30 10:41:41 +0530

    Add files via upload
    To use this code:
    
    Replace "YOUR_API_KEY" in the url variable with your actual OpenWeatherMap API key. You can sign up for a free API key at https://openweathermap.org/.
    
    1.Save the code to a Python file, e.g., weather_forecast.py.
    
    2.Open a terminal or command prompt, navigate to the directory where the file is saved, and execute the script by running python weather_forecast.py.
    
    3.Enter the name of the city for which you want to retrieve the weather forecast.
    
    4.The script will make a request to the OpenWeatherMap API, retrieve the weather data for the specified city, and display the weather, temperature, and humidity information. It includes error handling to handle cases where the API request fails or encounters an exception.
    
    Please note that you need to have the requests library installed in your Python environment. You can install it by running pip install requests in your terminal or command prompt.

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+
+def get_weather_forecast(city):
+    # OpenWeatherMap API URL
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=0325388298bc5457a9f303e3c647c056"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        # Check if the API response was successful
+        if response.status_code == 200:
+            # Extract relevant information from the response
+            weather = data['weather'][0]['main']
+            temperature = data['main']['temp']
+            humidity = data['main']['humidity']
+
+            # Print the weather forecast
+            print(f"Weather forecast for {city}:")
+            print(f"Weather: {weather}")
+            print(f"Temperature: {temperature} K")
+            print(f"Humidity: {humidity}%")
+        else:
+            print("Error occurred while fetching weather data. Please try again.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    city = input("Enter a city name: ")
+    get_weather_forecast(city)
